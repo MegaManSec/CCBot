880483dab666fd3b51c7579c8f0c90351b675f52
Joshua Rogers <jrogers@opera.com>
2023-11-20 18:15:05 +0100

    Fix systemd attribute.

--- install.sh
+++ install.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
 
 if [ "$#" -eq 1 ]; then
-    SLACK_WEBHOOK_URL=$1
+    SLACK_WEBHOOK_URL="$1"
 fi
 
+# Step 1: Create a service file for ccbot
 SERVICE_FILE="/etc/systemd/system/ccbot.service"
-echo "[Unit]" > $SERVICE_FILE
-echo "Description=ccbot Service" >> $SERVICE_FILE
-echo "After=network.target" >> $SERVICE_FILE
-echo "" >> $SERVICE_FILE
-echo "[Service]" >> $SERVICE_FILE
-echo "ExecStart=/usr/bin/python3 /usr/local/bin/ccbot.py" >> $SERVICE_FILE
-echo "Restart=always" >> $SERVICE_FILE
-echo "User=root" >> $SERVICE_FILE
-echo "Environment=SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL" >> $SERVICE_FILE
-echo "" >> $SERVICE_FILE
-echo "[Install]" >> $SERVICE_FILE
-echo "WantedBy=default.target" >> $SERVICE_FILE
+cat <<EOL > $SERVICE_FILE
+[Unit]
+Description=ccbot Service
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/python3 /usr/local/bin/ccbot.py
+Restart=always
+DynamicUser=yes
+Environment=SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL
+StandardOutput=append:/var/log/ccbot.log
+StandardError=append:/var/log/ccbot_error.log
+
+[Install]
+WantedBy=default.target
+EOL
 
 cp ccbot.py /usr/local/bin/ccbot.py
 chmod +x /usr/local/bin/ccbot.py
 
+# Step 3: Create a logrotate configuration file for ccbot
+LOGROTATE_FILE="/etc/logrotate.d/ccbot"
+cat <<EOL > $LOGROTATE_FILE
+/var/log/ccbot.log /var/log/ccbot_error.log {
+    daily
+    rotate 31
+    compress
+    missingok
+    notifempty
+    create 0640 nobody nogroup
+}
+EOL
+
+# Step 4: Reload systemd and start the ccbot service
 systemctl daemon-reload
 systemctl start ccbot
 
+# Step 5: Enable the service to start at boot
 systemctl enable ccbot
+
+echo "ccbot has been installed, the service is started, and log rotation is set up."
